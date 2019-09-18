68d74bebe1c0c51beee6982f13054d2754f070b8
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 18:30:04 +0000

    Wed Sep 18 18:30:04 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,23 +5,21 @@ from collections import Counter
 def checkout(skus):
     dict_prices = {"A":50, "B": 30, "C":20, "D": 15}
 
-    try:
-        if (isinstance(skus, str)) & (skus.isalpha()):
-            list_skus = list(skus.upper())
-            dict_counts = Counter(list_skus)
-            total_costs = []
-            for k,v in dict_counts.items():
-                if k=="A":
-                    total_costs.append(v*dict_prices[key])
+    if (isinstance(skus, str)) & (skus.isalpha()):
+        list_skus = list(skus.upper())
+        dict_counts = Counter(list_skus)
+        total_costs = []
+        for k,v in dict_counts.items():
+            if k=="A":
+                total_costs.append(v*dict_prices[k])
 
-                elif k=="B":
-                    total_costs.append(v*dict_prices[key])
+            elif k=="B":
+                total_costs.append(v*dict_prices[k])
 
-                else:
-                    total_costs.append(v*dict_prices[key])
-            return total_costs.sum()
-        else:
-            return -1
-    except:
+            else:
+                total_costs.append(v*dict_prices[k])
+        return sum(total_costs)
+    else:
         return -1
+
 
