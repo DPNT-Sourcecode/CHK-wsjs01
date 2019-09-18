8a3d466e57f7da7ac4ca8d92df40f9a88411da76
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 18:17:24 +0000

    Wed Sep 18 18:17:24 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,13 @@ import collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    list_skus = list(skus)
-    dict_counts = Counter(list_skus)
+    try:
+        if (isinstance(skus, str)) & (skus.isalpha()):
+            list_skus = list(skus.upper())
+            dict_counts = Counter(list_skus)
+        else:
+            return -1
+    except:
+        return -1
+
 
