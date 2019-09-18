8d984aafb6e4e24d9db2f854d649dcd6bddef38f
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 19:24:43 +0000

    Wed Sep 18 19:24:43 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -31,25 +31,25 @@ def checkout(skus):
                                 prod_count -= deal_count*3
 
                             total_costs.append(prod_count*dict_prices[k])
-                        # Incorporate special deals for B
-                        elif k=="B":
-                            prod_count = v
-                            while (prod_count // 2) > 0:
-                                deal_count = prod_count//2
-                                deal_cost = deal_count * 45
-                                total_costs.append(deal_cost)
-                                prod_count -= deal_count*2
-                            total_costs.append(prod_count*dict_prices[k])
                         # Incorporate special deals for E
                         elif k=="E":
                             prod_count = v
                             free_items = 0
                             if prod_count > 2:
                                 if prod_count % 3 == 0:
-                                    free_items = prod_count // 2
+                                    free_B_items = prod_count // 2
                                 else:
-                                    free_items = prod_count % 3
-                            prod_count = prod_count-free_items
+                                    free_B_items = prod_count % 3
+                            total_costs.append(prod_count*dict_prices[k])
+                        # Incorporate special deals for B
+                        elif k=="B":
+                            prod_count = v-free_B_items
+                            while (prod_count // 2) > 0:
+                                deal_count = prod_count//2
+                                deal_cost = deal_count * 45
+                                total_costs.append(deal_cost)
+                                prod_count -= deal_count*2
+
                             total_costs.append(prod_count*dict_prices[k])
                         # Every other item
                         else:
@@ -61,6 +61,7 @@ def checkout(skus):
             return 0
     except:
         return -1
+
 
 
 
