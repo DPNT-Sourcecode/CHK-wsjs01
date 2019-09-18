9f1f85fe46f60dddadc77ea03aad4e902779226e
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 18:37:13 +0000

    Wed Sep 18 18:37:13 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,36 +4,39 @@ from collections import Counter
 # skus = unicode string
 def checkout(skus):
     dict_prices = {"A":50, "B": 30, "C":20, "D": 15}
+    try:
+        # If input is a string and consists of only alphabetical chars
+        if (isinstance(skus, str)) & (skus.isalpha()):
+            list_skus = list(skus.upper())
+            dict_counts = Counter(list_skus)
+            total_costs = []
+            # For every item in basket, calculate cost
+            for k,v in dict_counts.items():
+                # Incorporate special deals for A
+                if k=="A":
+                    deal_count = v//3
+                    deal_cost = deal_count * 130
+                    total_costs.append(deal_cost)
 
-    # If input is a string and consists of only alphabetical chars
-    if (isinstance(skus, str)) & (skus.isalpha()):
-        list_skus = list(skus.upper())
-        dict_counts = Counter(list_skus)
-        total_costs = []
-        # For every item in basket, calculate cost
-        for k,v in dict_counts.items():
-            # Incorporate special deals for A
-            if k=="A":
-                deal_count = v//3
-                deal_cost = deal_count * 130
-                total_costs.append(deal_cost)
+                    non_deal_count = v-(deal_count*3)
+                    total_costs.append(non_deal_count*dict_prices[k])
+                # Incorporate special deals for B
+                elif k=="B":
+                    deal_count = v//2
+                    deal_cost = deal_count * 45
+                    total_costs.append(deal_cost)
 
-                non_deal_count = v-(deal_count*3)
-                total_costs.append(non_deal_count*dict_prices[k])
-            # Incorporate special deals for B
-            elif k=="B":
-                deal_count = v//2
-                deal_cost = deal_count * 45
-                total_costs.append(deal_cost)
-
-                non_deal_count = v-(deal_count*2)
-                total_costs.append(non_deal_count*dict_prices[k])
-            # Every other item
-            else:
-                total_costs.append(v*dict_prices[k])
-        return sum(total_costs)
-    else:
+                    non_deal_count = v-(deal_count*2)
+                    total_costs.append(non_deal_count*dict_prices[k])
+                # Every other item
+                else:
+                    total_costs.append(v*dict_prices[k])
+            return sum(total_costs)
+        else:
+            return -1
+    except:
         return -1
+
 
 
 
