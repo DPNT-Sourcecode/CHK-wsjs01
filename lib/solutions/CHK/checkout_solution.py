eeeb84b21303147062b421393ab70764b6641ab7
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 18:33:04 +0000

    Wed Sep 18 18:33:04 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,15 +11,26 @@ def checkout(skus):
         total_costs = []
         for k,v in dict_counts.items():
             if k=="A":
-                total_costs.append(v*dict_prices[k])
+                deal_count = v//3
+                deal_cost = deal_count * 130
+                total_costs.append(deal_cost)
+
+                non_deal_count = v-(deal_count*3)
+                total_costs.append(non_deal_count*dict_prices[k])
 
             elif k=="B":
-                total_costs.append(v*dict_prices[k])
+                deal_count = v//2
+                deal_cost = deal_count * 45
+                total_costs.append(deal_cost)
 
+                non_deal_count = v-(deal_count*2)
+                total_costs.append(non_deal_count*dict_prices[k])
+                
             else:
                 total_costs.append(v*dict_prices[k])
         return sum(total_costs)
     else:
         return -1
+
 
 
