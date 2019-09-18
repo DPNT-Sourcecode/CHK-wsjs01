0b8c1389584510276d6bef4ac9fd6ff72965c14d
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 20:32:15 +0000

    Wed Sep 18 20:32:15 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,24 +2,34 @@ from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def deal(prod_count, og_price, deal_mult, deal_price):
-
-    return list_costs
-def superdeal(prod_count, og_price, deal_mult, deal_price, super_mult, super_price):
-    costs=[]
-    prod_count = v
-    while (prod_count // super_mult) > 0:
-        superdeal_count = prod_count//super_mult
-        superdeal_cost = superdeal_count * super_price
-        costs.append(superdeal_cost)
-        prod_count -= superdeal_count*super_mult
-    while (prod_count // deal_mult) > 0:
-        deal_count = prod_count//deal_mult
-        deal_cost = deal_count * deal_price
-        costs.append(deal_cost)
-        prod_count -= deal_count*deal_mult
-    costs.append(prod_count*dict_prices[k])
-    return list_costs
+# IF I HAD MORE TIME I WOULD IMPLEMENT FUNCTIONS TO HANDLE DEAL STYLES
+# def deal(prod_count, og_price, deal_mult, deal_price):
+#
+#     return list_costs
+#
+# def bogo_deal(prod_count, og_price, deal_mult, deal_price):
+#
+#     return list_costs
+#
+# def bog_other_deal(prod_count, og_price, deal_mult, deal_price):
+#
+#     return list_costs
+#
+# def superdeal(prod_count, og_price, deal_mult, deal_price, super_mult, super_price):
+#     costs=[]
+#     prod_count = v
+#     while (prod_count // super_mult) > 0:
+#         superdeal_count = prod_count//super_mult
+#         superdeal_cost = superdeal_count * super_price
+#         costs.append(superdeal_cost)
+#         prod_count -= superdeal_count*super_mult
+#     while (prod_count // deal_mult) > 0:
+#         deal_count = prod_count//deal_mult
+#         deal_cost = deal_count * deal_price
+#         costs.append(deal_cost)
+#         prod_count -= deal_count*deal_mult
+#     costs.append(prod_count*dict_prices[k])
+#     return list_costs
 
 def checkout(skus):
     dict_prices = {"A":50, "B":30, "C":20, "D":15, "E":40, "F":10,
@@ -27,11 +37,11 @@ def checkout(skus):
                    "M":15, "N":40, "O":10, "P":50, "Q":30, "R":50,
                    "S":30, "T":20, "U":40, "V":50, "W":20, "X":90,
                    "Y":10, "Z":50}
-    dict_deal_map = {"A":2, "B":1, "C":0, "D":0, "E":4, "F":4,
-                   "G":0, "H":2, "I":0, "J":0, "K":1, "L":0,
-                   "M":4, "N":4, "O":0, "P":1, "Q":1, "R":4,
-                   "S":0, "T":0, "U":4, "V":2, "W":0, "X":0,
-                   "Y":0, "Z":0}
+    # dict_deal_map = {"A":2, "B":1, "C":0, "D":0, "E":4, "F":4,
+    #                "G":0, "H":2, "I":0, "J":0, "K":1, "L":0,
+    #                "M":4, "N":4, "O":0, "P":1, "Q":1, "R":4,
+    #                "S":0, "T":0, "U":4, "V":2, "W":0, "X":0,
+    #                "Y":0, "Z":0}
     try:
         # If skus is not empty, if it is, return 0
         if skus:
@@ -143,16 +153,29 @@ def checkout(skus):
                             prod_count = v
                             free_Q_items = prod_count // 3
                             total_costs.append(prod_count*dict_prices[k])
-                        elif k=='F':
+                        elif k=='U':
                             prod_count = v
                             free_items = 0
                             while prod_count > 0:
-                                prod_count -= 2
+                                prod_count -= 3
                                 if prod_count > 0:
                                     prod_count -= 1
                                     free_items += 1
                             item_count = v-free_items
-                            total_costs.append(item_count*dict_prices[k])                                                                                                                
+                            total_costs.append(item_count*dict_prices[k])
+                        elif k=="V":
+                            prod_count = v
+                            while (prod_count // 3) > 0:
+                                superdeal_count = prod_count//3
+                                superdeal_cost = superdeal_count * 130
+                                total_costs.append(superdeal_cost)
+                                prod_count -= superdeal_count*3
+                            while (prod_count // 2) > 0:
+                                deal_count = prod_count//2
+                                deal_cost = deal_count * 90
+                                total_costs.append(deal_cost)
+                                prod_count -= deal_count*2
+                            total_costs.append(prod_count*dict_prices[k])
                         # Every other item
                         else:
                             total_costs.append(v*dict_prices[k])
@@ -163,11 +186,3 @@ def checkout(skus):
             return 0
     except:
         return -1
-
-
-
-
-
-
-
-
