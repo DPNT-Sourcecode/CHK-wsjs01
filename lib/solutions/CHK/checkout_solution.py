2bf4ea7c2a22d239d5faa6c5532071b5b42d09fb
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 18:14:24 +0000

    Wed Sep 18 18:14:24 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,8 @@
-
+import collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    list_skus = list(skus)
+    dict_counts = Counter(list_skus)
+
